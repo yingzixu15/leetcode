3d247deec4a15bc2fd0cca3b2410df7821bace40
yingzixu15 <39885738+yingzixu15@users.noreply.github.com>
2022-06-06 12:33:15 -0400

    290. Word Pattern

--- src/WordPattern.java
+++ src/WordPattern.java
@@ -22,20 +22,5 @@ public class WordPattern {
             } else return false;
         }
         return true;
-//        Map<Character, String> patternMap = new HashMap<>();
-//        String[] words = s.split(" ");
-//        if (words.length != pattern.length())
-//            return false;
-//        for (int i = 0; i < words.length; i++) {
-//            Character c = pattern.charAt(i);
-//            if (patternMap.containsKey(c)) {
-//                if (!patternMap.get(c).equals(words[i])) {
-//                    return false;
-//                }
-//            } else {
-//                patternMap.put(c, words[i]);
-//            }
-//        }
-//        return true;
     }
 }
